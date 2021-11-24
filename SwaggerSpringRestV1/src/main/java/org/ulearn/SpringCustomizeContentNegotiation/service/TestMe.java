fc23ca1f110178bce9e4d51eedbc5cd7d4de2548
Sadek rahman <mds.rahman1@gmail.com>
2021-11-24 11:06:46 -0600

    test push

--- SwaggerSpringRestV1/src/main/java/org/ulearn/SpringCustomizeContentNegotiation/service/TestMe.java
+++ SwaggerSpringRestV1/src/main/java/org/ulearn/SpringCustomizeContentNegotiation/service/TestMe.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.ulearn.SpringCustomizeContentNegotiation.model.City;
 import org.ulearn.SpringCustomizeContentNegotiation.model.CityList;
-// My Java doc
-public interface CityService {
+
+public interface TestMe {
 
 	public City getCity(String id);
 	public CityList getCities();
